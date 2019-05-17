2546217f74ae36ff6ca39266b03b6977051de75c
Erizta Alifa Prabandani <33772785+erizta@users.noreply.github.com>
2019-05-17 21:00:06 +0700

    Update dashboard.php

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,12 +1,129 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Dashboard Admin</title>
-</head>
-<body>
-	<h1>Hi Admin!</h1>
-</body>
-</html>
+
+<!-- Begin Page Content -->
+
+<div class="container-fluid">
+	<h1 class="h3 mb-2 text-gray-800"><?= $title; ?></h1>
+
+	<!-- Content Row -->
+	<div class="row">
+
+  <!-- Member Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-danger shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1" style="font-size: 17px">Jumlah Member</div>
+            <?php
+              $query=$this->db->query("SELECT * FROM member");
+              $jml=$query->num_rows();
+            ?>
+            <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 25px"><?= $jml; ?></div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-users fa-3x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Company Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-success text-uppercase mb-1" style="font-size: 17px">Jumlah Company</div>
+            <?php
+                $query=$this->db->query("SELECT * FROM perusahaan");
+                $jml=$query->num_rows();
+            ?>
+            <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 25px"><?= $jml; ?></div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-building fa-3x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Skills Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-success text-uppercase mb-1" style="font-size: 17px">Jumlah Keahlian</div>
+            <?php
+                $query=$this->db->query("SELECT * FROM list_keahlian");
+                $jml=$query->num_rows();
+            ?>
+            <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 25px"><?= $jml; ?></div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-network-wired fa-3x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Skills Category Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-warning shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1" style="font-size: 17px">Jumlah Kategori Keahlian</div>
+            <?php
+                $query=$this->db->query("SELECT * FROM kategori_keahlian");
+                $jml=$query->num_rows();
+            ?>
+            <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 25px"><?= $jml; ?></div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-briefcase fa-3x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Skills Category Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-primary shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" style="font-size: 17px">Jumlah Portofolio</div>
+            <?php
+                $query=$this->db->query("SELECT * FROM portofolio");
+                $jml=$query->num_rows();
+            ?>
+            <div class="h5 mb-0 font-weight-bold text-gray-800" style="font-size: 25px"><?= $jml; ?></div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-file-alt fa-3x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+	</div>
+
+	<!-- Content Row -->
+
+
+	<!-- Content Row -->
+	<div class="row">
+
+		<!-- Content Column -->
+		<div class="col-lg-6 mb-4">
+
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
