e1e1950b065c18a4f84875709f6325411b33c700
Erizta Alifa Prabandani <33772785+erizta@users.noreply.github.com>
2019-05-17 21:00:33 +0700

    Update Admin_model.php

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -1,5 +1,9 @@
 <?php
 
 class Admin_model extends CI_Model{
-    
+  function hapus_member($id){
+      $query=$this->db->query("delete from member where id_user='$id'");
+      return $query;
+  }
+
 }
