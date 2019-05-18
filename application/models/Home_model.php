6276cd6fc27f1a9372e1f6fee00a7dc171809ffa
Erizta Alifa Prabandani <33772785+erizta@users.noreply.github.com>
2019-05-19 00:19:37 +0700

    Update Home_model.php

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -2,4 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home_model extends CI_Model {
+
+  public function getAllService()
+  {
+      $this->db->from('service');
+      $query = $this->db->get();
+      return $query->result_array();
+  }
+  
 }
